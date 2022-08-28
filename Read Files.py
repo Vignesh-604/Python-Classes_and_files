6a60a645c07cd5d6c26390f385e258a04f68b2e5
Vignesh H <110926281+Vignesh-604@users.noreply.github.com>
2022-08-28 13:51:55 +0530

    Updated 22/08/22

--- Read Files.py
+++ Read Files.py
@@ -1,17 +1,22 @@
 # r = read only , w = writeable , a = append/add new text without modifying previous entries , r+ = read and writeable.
 
-folder = open("Animals.txt", "r")
+folder = open("Animals.txt", "r")                  # open(file_name, file_mode)
 
 def Read():
-    print(folder.readable())
-    print(folder.readline())
-    print(folder.readline())
-    print(folder.readlines()[1])
-    # print(Coolio.readlines()) -> Will print every item n the file.
+    print(folder.readable())                       # Checks if the file is the readable or not. Depends on the mode specified before.
 
-def Loop():
+    print(folder.readline())                       # Reads the first line. Once the line is printed, it won't be taken into consideration.
+
+    print(folder.readline())                       # Reads the second line. Since these lines aren't considered, their indexes are also changed.
+
+    print(folder.readlines()[1])                   # Since indexes are changed, it'll count the lines which aren't read. Can be used only once.
+
+    #print(folder.readlines())                      # Will print every item in a list. If the code is wriiten before, this will print an empty list.
+
+def Loop():                                         # Prints each item.
     for animal in folder:
         print(animal)
 
+#Loop()
 Read()
 folder.close()
