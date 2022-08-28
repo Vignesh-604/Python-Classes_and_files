4a773253574da4caf14191ebe173f539ea433a8c
Vignesh H <110926281+Vignesh-604@users.noreply.github.com>
2022-08-28 13:29:24 +0530

    Updated 28/08/22

---
+++ Object Functions.py
@@ -0,0 +1,17 @@
+from Class import Animals
+
+Catto = Animals("Cat", 10, "Bear")
+Doggo = Animals("Dog", 7, "Monkey")
+print(Doggo.rate())                                             # Compares the rate as specified in the function.
+print(Catto.rate())
+
+from Class import Animals
+
+Cat = Animals("Catto", 10, "Bear")
+Dog = Animals("Doggo", 7, "Monkey")
+print(Cat.rating, Dog.fren, Cat.pet)
+
+from Class import Animals_2
+
+Bear = Animals_2(True, "Fish", "Bear")
+print(Bear.wild and Bear.ani)
