3debf31df23e42dd23b6840e04276302a9887ac9
Vignesh H <110926281+Vignesh-604@users.noreply.github.com>
2022-08-28 13:11:08 +0530

    Updated 28/08/22

--- Write Files.py
+++ Write Files.py
@@ -1,16 +1,16 @@
-# Write deletes everything inm the folder and writes the new input. Use "a" in the open file and then insert in line.
+# 'Write' deletes everything in the folder and writes the new input. Use "a" in the open file and then insert in line.
 
 Folder = open("Animals.txt", "a")
 
-def Write():
+def Write():                                               # This will Open the specified file and print in it.
     (Folder.write("Rat"))
     print(Folder.write("\nHen"))
 
 def Write1():
-    Folder_2 = open("Birbs.txt", "w")
+    Folder_2 = open("Birbs.txt", "w")                      # This will open a text file and print the given in it.
     Folder_2.write("Hawk")
 
-def Write2():
+def Write2():                                              # This will make a HTML file and the given in it.
     HTML = open("insect.html", "w")
     HTML.write("<p> HTML Bugsies </p>")
 
